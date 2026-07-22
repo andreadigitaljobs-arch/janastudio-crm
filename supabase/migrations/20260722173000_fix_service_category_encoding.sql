cfd5e9df5b10cdda9e43abdb5425fa15c34c920d
inmunovzla-ux <inmunovzla@gmaiil.com>
2026-07-22 11:41:35 -0400

    Repair service category labels

---
+++ supabase/migrations/20260722173000_fix_service_category_encoding.sql
@@ -0,0 +1,11 @@
+begin;
+
+update janastudio.services
+set category = 'Uñas'
+where category = 'U??as';
+
+update janastudio.services
+set category = 'Pestañas'
+where category = 'Pesta??as';
+
+commit;
